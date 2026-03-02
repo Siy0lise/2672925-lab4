1abdf4687232303e6fda604b18cb104fccf3ac9b
Siy0lise <siyolisedlani@gmail.com>
2026-03-02 16:28:22 +0200

    Fixed fetch to get full API Endpoint

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function searchCountry(countryName) {
         borderSection.innerHTML = "";
 
         //Fetch country data
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         if (!response.ok){
             throw new Error("Country not found");
         }
